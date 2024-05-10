8615191441484b1db48a8ff040d18db2c07b0a9e
Karthikeyan Ramesh <karthikramesh444@gmail.com>
2024-05-10 09:32:35 +0530

    untracked css files added

---
+++ Scripts/Script.js
@@ -0,0 +1,54 @@
+
+
+// Primary header functionalities
+function country_dropdown_handler() {
+    const current_state = document.querySelector(".countries-dropdown");
+
+    if (current_state.style.visibility === "visible") {
+        current_state.style.visibility = "hidden";
+    } else {
+        current_state.style.visibility = "visible";
+    }
+}
+
+function mail_dropdown_handler() {
+    const current_state = document.querySelector(".mail-to-dropdown");
+
+    if (current_state.style.visibility === "visible") {
+        current_state.style.visibility = "hidden";
+    } else {
+        current_state.style.visibility = "visible";
+    }
+}
+
+function notification_dropdown_handler() {
+    const current_state = document.querySelector(".notifications-dropdown");
+
+    if (current_state.style.visibility === "visible") {
+        current_state.style.visibility = "hidden";
+    } else {
+        current_state.style.visibility = "visible";
+    }
+}
+
+function profile_dropdown_handler() {
+    const current_state = document.querySelector(".profile-dropdown");
+
+    if (current_state.style.visibility === "visible") {
+        current_state.style.visibility = "hidden";
+    } else {
+        current_state.style.visibility = "visible";
+    }
+}
+
+
+// Secondary header functionalities 
+function settings_dropdown_handler() {
+    const current_state = document.querySelector(".settings-dropdown");
+
+    if (current_state.style.visibility === "visible") {
+        current_state.style.visibility = "hidden";
+    } else {
+        current_state.style.visibility = "visible";
+    }
+}
