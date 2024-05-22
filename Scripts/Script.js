d6815ca4dd555ca86ca6569c2e2666475c940b68
Karthikeyan Ramesh <karthikramesh444@gmail.com>
2024-05-22 22:40:20 +0530

    notes app initial commit

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -1,15 +1,29 @@
 
 $(document).ready(function () {
+  // SideBar handler
+    $('.sidebar-menu-button').click(function() {
+        $('.sidebar-container').toggleClass("sidebar-close");
+        $('.main-container').toggleClass('expand');
+      });
+
+  // Dropdown handler
+    $('.btn-action').click(function() {
+        $(this).parents('.dropdown-container').toggleClass('open_dropdown');
+      });
+      
+    })
     
-      // SideBar handler
-        $('.sidebar-menu-button').click(function() {
-            $('.sidebar-container').toggleClass("sidebar-close");
-            $('.main-container').toggleClass('expand');
-        });
 
-      // Dropdown handler
-        $('.btn-action').click(function() {
-            $(this).parents('.dropdown-container').toggleClass('open_dropdown');
-        });
-})
-
+    
+// change page 
+function navigateTo(page) {
+  $.ajax({
+    url: page + ".html",
+    success: function (result) {
+      $(".main-container").html(result)
+    },
+    error: function (err) {
+      console.log(err)
+    }
+  });
+};
